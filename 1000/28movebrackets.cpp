48ecaa6ec97c9c9ec8ea4c4c1787e5336db4b76c
Sumit Ranjan <sumitran1234@gmail.com>
2025-08-08 23:16:11 +0530

    new soln

--- 1000/28movebrackets.cpp
+++ 1000/28movebrackets.cpp
@@ -15,7 +15,27 @@ using namespace std;
 
 
 void solve(){
-    
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int dis=0;
+    int plus=0;
+    for(int i=0; i<n; i++){
+        if(s[i]='('){
+            plus++;
+        }
+        else{
+            if(plus>0){
+                plus--;
+            }
+            else{
+                dis++;
+            }
+        }
+
+    }
+    cout<<dis<<endl;
 }
 
 
