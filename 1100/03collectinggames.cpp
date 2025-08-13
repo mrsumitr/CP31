4dfeba6adfb36f299f4f73d1f01ccd6f720c2698
Sumit Ranjan <sumitran1234@gmail.com>
2025-08-13 23:25:59 +0530

    update

--- 1100/03collectinggames.cpp
+++ 1100/03collectinggames.cpp
@@ -15,8 +15,58 @@ using namespace std;
 
 
 void solve(){
+    ll n;
+    cin>>n;
+    vector<pair<ll,ll>> v(n);
+    for(int i=0; i<n; i++){
+        cin>>v[i].first;
+        v[i].second=i;
+    }
+    if(n==1){
+        cout<<"0"<<endl;
+        return;
+    }
+    sort(v.begin(),v.end());
     
-}
+    vector<int> ans(n);
+    int s=0;
+    int e=1;
+    ll sum=v[0].first;
+    int ele=0;
+    while(s<=e && s<n && e<n){
+        
+       if(sum>=v[e].first){
+        sum+=v[e].first;
+        ele++;
+        e++;
+       }
+       else{
+        while(s<e-1){
+            ans[v[s].second]=ele;
+            s++;
+        }
+        ans[v[s].second]=ele;
+        sum+=v[e].first;
+        ele++;
+        s++;
+        e++;
+       }
+       
+    }
+    while(s<n){
+        ans[v[s].second]=ele;
+        s++;
+       }
+       for(int i=0; i<n; i++){
+        cout<<ans[i]<<" ";
+       }
+       cout<<endl;
+// sort(v.begin(),v.end(),[](const pair<int,int> &a,pair<int,int> &b){
+//         if(a.first==b.first)
+//             return b.second>a.second;
+//         return a.first<b.first;
+// });
+    }
 
 
    
