e187344805636b9ba7bea901ecb21ed79a969e6e
Sumit Ranjan <sumitran1234@gmail.com>
2025-09-19 16:37:07 +0530

    done

--- 1100/26eatingcandies.cpp
+++ 1100/26eatingcandies.cpp
@@ -15,7 +15,47 @@ using namespace std;
 
 
 void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+    }
     
+    if(n==1){
+        cout<<"0"<<endl;
+        return;
+    }
+    if(n==2){
+        if(v[0]==v[1]){
+            cout<<"2"<<endl;
+        }
+        else{
+            cout<<"0"<<endl;
+        }
+        return;
+    }
+    ll left=0;
+    ll right=n-1;
+    ll weight1=v[0];
+    ll weight2=v[right];
+    ll candies=0;
+    while(left<right){
+        if(weight1==weight2){
+            candies=left+1+n-right;
+        }
+        if(weight1<=weight2){
+            left++;
+            weight1+=v[left];
+        }
+        else{
+            right--;
+            weight2+=v[right];  
+        }
+        
+    }
+    cout<<candies<<endl;
+
 }
 
 
