8cedb26e094121aba52dfbe9a4c7f6be1e82a9b8
Sumit Ranjan <sumitran1234@gmail.com>
2025-10-02 19:03:50 +0530

    potd

--- 1200/05vikaandbrdige.cpp
+++ 1200/05vikaandbrdige.cpp
@@ -8,21 +8,47 @@ using namespace std;
 
 void solve() {
 
-    ll n,k;
+    int n,k;
     cin>>n>>k;
-    vector<ll> v(n);
+    vector<int> v(n);
     for(int i=0; i<n; i++){
          cin>>v[i];
-    }
-    if(k==1){
-        cout<<"0"<<endl;
-        return;
-    }
-    
+   
+    }  
+    vector<int> last(k,-1);
+    vector<int> max_step(k),max_step2(k);
 
-    
-    
+    for(int i=0; i<n; i++){
+        int step=i-last[v[i]-1];
+        if(step>max_step[v[i]-1]){
+            max_step2[v[i]-1]=max_step[v[i]-1];
+            max_step[v[i]-1]=step;
+
+        }
+        else if(step>max_step2[v[i]-1]){
+            max_step2[v[i]-1]=step;
+
+        }
+        last[v[i]-1]=i;
+    }
+
+    for(int i=0; i<k; i++){
+        int step=n-last[i];
+        if(step>max_step[v[i]-1]){
+             max_step2[i]=max_step[i];
+             max_step[i]=step;
+        }
+        else if(step>max_step2[i]){
+            max_step2[i]=step;
+        }
+    }
+    int ans=1e9;
+    for(int i=0; i<k; i++){
+        ans=min(ans,max((max_step[i]+1)/2,max_step2[i]));
+    }
+    cout<<ans<<nline;
 }
+
 
 int main() {
     ios::sync_with_stdio(false);
