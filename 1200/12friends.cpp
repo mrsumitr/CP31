dca2ed6877d806973681775f32967685af988af9
Sumit Ranjan <sumitran1234@gmail.com>
2025-10-03 10:44:38 +0530

    potd

--- 1200/12friends.cpp
+++ 1200/12friends.cpp
@@ -10,6 +10,52 @@ using namespace std;
 
 void solve() {
     
+    ll n;
+    cin>>n;
+    vector<ll> x(n);
+    vector<ll> y(n);
+    vector<ll> pos,neg;
+    for(int i=0; i<n; i++) cin>>x[i];
+    for(int i=0; i<n; i++) cin>>y[i];
+    for(int i=0; i<n; i++){
+        if(y[i]-x[i]<0){
+            neg.push_back(abs(y[i]-x[i]));
+        }
+        else{
+            pos.push_back(y[i]-x[i]);
+        }
+    }
+    sort(pos.rbegin(),pos.rend());
+    sort(neg.rbegin(),neg.rend());
+    ll ans=0;
+    if(pos.empty()){
+        cout<<"0"<<endl;
+        return;
+    }
+    if(neg.empty()){
+        cout<<pos.size()/2<<endl;
+        return;
+    }
+    
+    int posi=0;
+    int negi=0;
+    while(posi<pos.size() && negi<neg.size()){
+        if(pos[posi]>=neg[negi]){
+            ans++;
+            negi++;
+            posi++;
+        }
+        else{
+            negi++;
+        }
+    }
+    int size=pos.size()-posi;
+    if(size>0){
+        ans+=size/2;
+
+    }
+    cout<<ans<<endl;
+    
 
     
 }
