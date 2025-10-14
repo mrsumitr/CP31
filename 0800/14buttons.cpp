ebcdc103294963f908914cd94386cc32ed8ddb24
Sumit Ranjan <sumitran1234@gmail.com>
2025-10-15 01:03:41 +0530

    potd

--- 0800/14buttons.cpp
+++ 0800/14buttons.cpp
@@ -7,15 +7,23 @@ void solve(){
     while(t--){
         long long a,b,c;
         cin>>a>>b>>c;
-        long long num=a+b+c;
-        if(num%2==1){
-            cout<<"First"<<endl;
+         if (c % 2 == 1) // odd
+        {
+            if (b > a) // 1
+                cout << "Second" << endl;
+            else
+                cout << "First" << endl;
         }
-        else{
-            cout<<"Second"<<endl;
+        else // even
+        {
+            if (a > b) // 1
+                cout << "First" << endl;
+            else
+                cout << "Second" << endl;
         }
     }
-}
+    }
+
 int main(){
     solve();
 }
