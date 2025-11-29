4cf9a52e0f7ab6134473cac3a429f99942f5f0d9
Sumit Ranjan <sumitran1234@gmail.com>
2025-11-29 11:01:56 +0530

    potd

--- 1300/14arrayele.cpp
+++ 1300/14arrayele.cpp
@@ -1,4 +1,4 @@
-//#include<bits/stdc++.h>
+#include<bits/stdc++.h>
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -10,8 +10,45 @@ using namespace std;
 int mod=1e9+7;
 void mrsumit() {
     
-    
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++) cin>>v[i];
+    vector<int> ans;
+    int gc=0;
 
+    for(int i=0; i<32; i++ ){
+        
+        int bit=1<<i;
+        int cnt=0;
+        for(int j=0; j<n; j++){
+            if(v[j]&bit){
+                cnt++;
+            }
+        }
+        if(gc){
+            gc=__gcd(gc,cnt);
+        }
+        else{
+            gc=cnt;
+        }
+    }
+    if (gc == 0) {
+    for (int i = 1; i <= n; i++) {
+        cout << i << " ";
+    }
+    cout << nline;
+    return;
+}
+    for(int i=1; i<=gc; i++){
+        if(gc%i==0){
+            ans.push_back(i);
+        }
+    }
+    for(auto it: ans){
+        cout<<it<<" ";
+    }
+    cout<<nline;
 
 }
 
