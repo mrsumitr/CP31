2e948b021399cfc8aee4d4241714127ac2b58d1a
Sumit Ranjan <sumitran1234@gmail.com>
2026-01-15 10:21:12 +0530

    complete

--- 1300/18moveandturn.cpp
+++ 1300/18moveandturn.cpp
@@ -9,14 +9,28 @@ using namespace std;
 #define nline '\n'
 int mod=1e9+7;
 void mrsumit() {
-    
+    int n;
+    cin>>n;
+    ll ans=0;
+    if(n==1){
+        cout<<4<<endl;
+        return;
+    }
+    if(n%2){
+        ans=2*(n/2+2)*(n/2+1);
+    }
+    else{
+        ans=(n/2+1)*(n/2+1);
+    }
+    cout<<ans<<'\n';
+    // use comination to solve this, like n/2  
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin >> t;
+    int t=1;
+    //cin >> t;
     while (t--) {
         mrsumit();
     }
