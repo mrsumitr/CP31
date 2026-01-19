73f31dd376b766ddbf0b0902d553c79d7c60b547
Sumit Ranjan <sumitran1234@gmail.com>
2026-01-19 10:53:27 +0530

    optimized soln

--- 1400/04arraygame.cpp
+++ 1400/04arraygame.cpp
@@ -1,79 +1,62 @@
-#include<bits/stdc++.h>
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<set>
-using namespace std;
-#include<map>
-#define ll long long
-#define nline '\n'
-int MOD=1e9+7;
-void mrsumit() {
-    int n,k;
-    cin>>n>>k;
-    vector<ll> v;
-    map<ll,ll> mp;
-    bool ans=false;
-    for(int i=0; i<n; i++){
-      int data;
-      cin>>data;
-      v.push_back(data);
-      mp[data]++;
-      if(mp[data]>1){
-        ans=true;
-      }
-    }
-    if(ans){
-      cout<<0<<endl;
-      return;
-    }
-    if(k>=3){
-      cout<<0<<endl;
-      return;
-    } 
-    sort(v.begin(),v.end());
-    ll minv=v[0];
-    vector<int> arr;
-    bool 2n=false;
-    for(int i=0; i<n-1; i++){
-      for(int j=i+1; k<n; j++){
-        ll diff=v[j]-v[i];
-        arr.push_back(diff);
-        if(mp.find(diff)!=mp.end()){
-          2n=true;
-        }
-        minv=min(minv,diff);
-      }
-    }
-    if(k==1){
-      cout<<minv<<endl;
-      return;
-    }
-    if(2n){
-      cout<<0<<endl;
-    }
-    else{
-      for(int i=0; i<(int)arr.size(); i++){
-          for(int j=0; j<(int)v.size(); j++){
-            ll diff=abs(v[i]-arr[j]);
-            minv=min(minv,diff);
-          }
-      }
-    }
-    cout<<minv<<endl;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
 
-    
-    
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    if (k >= 3) {
+        cout << 0 << endl;
+        return;
+    }
+
+    sort(v.begin(), v.end());
+
+    ll min_val = v[0];
+    for (int i = 0; i < n - 1; i++) {
+        min_val = min(min_val, v[i + 1] - v[i]);
+    }
+
+    if (k == 1) {
+        cout << min_val << endl;
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            ll d = v[j] - v[i];
+            
+            
+            auto it = lower_bound(v.begin(), v.end(), d);
+            
+         
+            if (it != v.end()) {
+                min_val = min(min_val, *it - d);
+            }
+            // Check the element before 'it' (< d)
+            if (it != v.begin()) {
+                min_val = min(min_val, d - *prev(it));
+            }
+        }
+    }
+
+    cout << min_val << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
+
     int t;
     cin >> t;
     while (t--) {
-        mrsumit();
+        solve();
     }
     return 0;
 }
