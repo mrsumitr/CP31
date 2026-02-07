7d1c87892299eeea629229a31ec06266045a4e21
Sumit Ranjan <sumitran1234@gmail.com>
2026-02-07 11:25:12 +0530

    potd

--- 1400/16arrangesheep.cpp
+++ 1400/16arrangesheep.cpp
@@ -9,7 +9,31 @@ using namespace std;
 #define nline '\n'
 int mod=1e9+7;
 void mrsumit() {
-    
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    vector<int> pos;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '*') pos.push_back(i);
+    }
+
+    if (pos.empty()) {
+        cout << 0 << endl;
+        return;
+    }
+
+    int mid_idx = pos.size() / 2;
+    int mid_pos = pos[mid_idx];
+    long long ans = 0;
+
+    for (int i = 0; i < pos.size(); i++) {
+        int target_pos = mid_pos - (mid_idx - i);
+        ans += abs(pos[i] - target_pos);
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
